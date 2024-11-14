fb6ff750b3cb8aa110fa0bb14967dd52ba4b8bdb
210410055 <taiwanhero230@gmail.com>
2024-11-14 21:38:21 +0800

    w02 fix

--- demo/w07_55.js
+++ demo/w07_55.js
@@ -3,16 +3,16 @@ console.log('show-classdemo', p)
 
 function showDemo(item) {
   switch (item) {
-    case 'w02_intro_xx':
-      p.innerHTML = `<iframe src='./w02_intro_55/w02.html' width="100%" height="100%" />`
+    case 'w02_intro_55':
+      p.innerHTML = `<iframe src='./w02-intro-55/w02.html' width="100%" height="100%" />`
       break
-    case 'w03_card_xx':
+    case 'w03_card_55':
       p.innerHTML = `<iframe src='./w03_card_55/index.html' width="100%" height="100%" />`
       break
-    case 'w06_blog_xx':
+    case 'w06_blog_55':
       p.innerHTML = `<iframe src='./w06_blog_55/index.html' width="100%" height="100%" />`
       break
-    case 'w09_quiz1_xx':
+    case 'w09_quiz1_':
       p.innerHTML = `<iframe src='./w09_quiz1_xx/p5_xx.html' width="100%" height="100%" />`
       break
   }
