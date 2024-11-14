46927136594283c8096c5c2600b5154db78f1c09
210410055 <taiwanhero230@gmail.com>
2024-11-14 21:36:07 +0800

    fix w07 css

--- demo/w07_55.js
+++ demo/w07_55.js
@@ -4,13 +4,13 @@ console.log('show-classdemo', p)
 function showDemo(item) {
   switch (item) {
     case 'w02_intro_xx':
-      p.innerHTML = `<iframe src='./w02_intro_xx/index.html' width="100%" height="100%" />`
+      p.innerHTML = `<iframe src='./w02_intro_55/w02.html' width="100%" height="100%" />`
       break
     case 'w03_card_xx':
-      p.innerHTML = `<iframe src='./w03_card_xx/' width="100%" height="100%" />`
+      p.innerHTML = `<iframe src='./w03_card_55/index.html' width="100%" height="100%" />`
       break
     case 'w06_blog_xx':
-      p.innerHTML = `<iframe src='./w06_blog_xx/' width="100%" height="100%" />`
+      p.innerHTML = `<iframe src='./w06_blog_55/index.html' width="100%" height="100%" />`
       break
     case 'w09_quiz1_xx':
       p.innerHTML = `<iframe src='./w09_quiz1_xx/p5_xx.html' width="100%" height="100%" />`
